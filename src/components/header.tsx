f3f29284c7dfe7211fc45f042e569ae9dd8e671e
Harley Knox <therealharleyknox@gmail.com>
2025-07-25 08:17:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,30 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { Sparkles } from 'lucide-react';
+import { Sparkles, ChevronDown } from 'lucide-react';
 import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { categories } from '@/lib/data';
+import React from 'react';
+
+const mainCategories = [
+  'makeup',
+  'skincare',
+  'hair',
+  'pleasure',
+  'protection',
+  'entrepreneurial',
+];
+const mainCats = categories.filter((c) => mainCategories.includes(c.slug));
+const moreCats = categories.filter((c) => !mainCategories.includes(c.slug));
 
 export function Header() {
   return (
@@ -14,7 +36,63 @@ export function Header() {
             304Beauty Navigator
           </span>
         </Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {mainCats.map((category) => (
+              <NavigationMenuItem key={category.slug}>
+                <Link href={`/${category.slug}`} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {category.name}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>More</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {moreCats.map((component) => (
+                    <ListItem
+                      key={component.name}
+                      title={component.name}
+                      href={`/${component.slug}`}
+                    >
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </header>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = 'ListItem';
